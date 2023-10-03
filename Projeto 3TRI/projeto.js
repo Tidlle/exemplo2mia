76d169976a63a631d50497c7d4932ae1404ba8e8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-03 11:15:58 -0300

    PROJETO 3TRI

---
+++ Projeto 3TRI/projeto.js
@@ -0,0 +1,12 @@
+const pacientes = [];
+const colaboradores = [];
+
+// Função para adicionar um paciente
+function adicionarPaciente(paciente) {
+    pacientes.push(paciente);
+}
+
+// Função para adicionar um colaborador/psicólogo
+function adicionarColaborador(colaborador) {
+    colaboradores.push(colaborador);
+}
